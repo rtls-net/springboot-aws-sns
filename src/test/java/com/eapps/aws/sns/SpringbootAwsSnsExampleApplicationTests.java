9315aa2f124ff65ef1e4dd13009b4da7320ceae4
udit <uditkumar063@gmail.com>
2023-10-15 02:10:22 +0530

    first springboot aws sns repo setup

---
+++ src/test/java/com/eapps/aws/sns/SpringbootAwsSnsExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eapps.aws.sns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAwsSnsExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
